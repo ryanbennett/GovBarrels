74f40e5fead504931fd9917229d1951a7cb8ff72
Bennett, Ryan <ryanjbennett@live.com>
2020-06-24 11:05:59 -0500

    Add project files.

---
+++ GovernmentBarrels/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GovernmentBarrels.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+    }
+}
